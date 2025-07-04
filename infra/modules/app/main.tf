2c72de81b0351ffa91fcc201390310e702469ee7
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-07-04 11:44:54 -0400

    Update main.tf

--- infra/modules/app/main.tf
+++ infra/modules/app/main.tf
@@ -1,28 +1,33 @@
-resource "google_cloud_run_service" "default" {
-  name     = var.name
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_cloud_run_service" "app" {
+  name     = "flask-${var.env}"
   location = var.region
 
   template {
     spec {
       containers {
-        image = var.image
-        env {
-          name  = "ENV"
-          value = var.environment
-        }
+        image = var.image_url
       }
     }
   }
 
-  traffic {
+  traffics {
     percent         = 100
     latest_revision = true
   }
 }
 
-resource "google_cloud_run_service_iam_member" "invoker" {
-  service  = google_cloud_run_service.default.name
-  location = var.region
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+resource "google_cloud_deploy_target" "target" {
+  name      = var.env
+  location  = var.region
+  target_id = var.env
+
+  run {
+    location = google_cloud_run_service.app.location
+    service  = google_cloud_run_service.app.name
+  }
 }
